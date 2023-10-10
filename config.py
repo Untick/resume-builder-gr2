876f09db8bebcd55ecb255342a051f5421693bf7
BuildTools <unconfigured@null.spigotmc.org>
2023-10-10 18:18:20 +0300

    -

--- config.py
+++
@@ -1,3 +0,0 @@
-TG_BOT_TOKEN = '6607449443:AAEpi5wFX8GAGPnYXF3wmwk_G-OtPUd38r0'  # токен тг-бота, подставить свой
-TG_API_TOKEN = 'WJyPjWJOGb03YqMVzdl5hHwRncMx7P7j'  # токен для внешних запросов по API для вызова команд тг-бота
-OPENAI_API_KEY = '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'
